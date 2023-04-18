58f8ac09686fbbbb5c4ef5750b480edef3c7c53d
Tomasz Kamiński <104736559+tomm26177@users.noreply.github.com>
2023-04-18 21:29:03 +0200

    Update wektorpotegi.R
    m

--- MSzS2lab01/wektorpotegi.R
+++ MSzS2lab01/wektorpotegi.R
@@ -1,7 +1,9 @@
 n=1:20
 n
-w=2^n
+w=2^n  #Tworzy wektor w, którego wartości to kolejne potęgi liczby 2 dla elementów wektora n
 w
-cj=w%%10
+cj=w%%10  #Tworzy wektor cj, który zawiera reszty z dzielenia elementów wektora w przez 10.
 cj
-summary(factor(cj))
+summary(factor(cj)) #Tworzy podsumowanie kategoryczne dla wektora cj.
+#Funkcja factor konwertuje wektor na zmienną kategoryczną,
+#a następnie funkcja summary podaje liczebność i procentowy udział poszczególnych kategorii.
