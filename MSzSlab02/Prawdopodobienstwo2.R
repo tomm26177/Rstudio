8e05bdf96a7d46ba86e4302dd2d1cf964dac099c
Tomasz Kamiński <104736559+tomm26177@users.noreply.github.com>
2023-04-18 21:31:32 +0200

    Update Prawdopodobienstwo2.R
    m

--- MSzSlab02/Prawdopodobienstwo2.R
+++ MSzSlab02/Prawdopodobienstwo2.R
@@ -1,3 +1,5 @@
 #kwantyl rzędu p1 (inne p niż parametr rozkladu)
 p1=0.75
-qbinom(p1,n,p)
+qbinom(p1,n,p) #funkcja qbinom zwróci wartość zmiennej losowej binominalnej, 
+#która opisuje liczbę sukcesów w n niezależnych próbach Bernoulliego, 
+#gdzie każda próba ma prawdopodobieństwo sukcesu równe p.
