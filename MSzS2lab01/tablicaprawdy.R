edfe0f71e033e8483057c2125ce4fdaecad5fc53
Tomasz Kamiński <104736559+tomm26177@users.noreply.github.com>
2023-04-18 21:02:20 +0200

    Update tablicaprawdy.R
    cc

--- MSzS2lab01/tablicaprawdy.R
+++ MSzS2lab01/tablicaprawdy.R
@@ -1,3 +1,7 @@
 a=c(0,1,0,1)
 b=c(0,0,1,1)
 data.frame(b,a, or=a|b, and=a&b, nie_a=!a)
+#Ten kod tworzy ramkę danych z kolumnami b i a, a następnie dodaje trzy dodatkowe kolumny:
+# or: Ta kolumna oblicza operację logicznego OR między a i b. Zwraca wartość TRUE, jeśli jedno z a lub b jest TRUE, a w przeciwnym przypadku zwraca wartość FALSE.
+# and: Ta kolumna oblicza operację logicznego AND między a i b. Zwraca wartość TRUE tylko wtedy, gdy oba a i b są TRUE, a w przeciwnym przypadku zwraca wartość FALSE.
+# nie_a: Ta kolumna neguje wartości w wektorze a. Zwraca TRUE, jeśli wartość w a jest równa 0, a w przeciwnym przypadku zwraca FALSE.
