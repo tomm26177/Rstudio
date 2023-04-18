c459bb3fde9bf774d7c2ecfb86f99c01abbb74f7
Tomasz Kamiński <104736559+tomm26177@users.noreply.github.com>
2023-04-18 21:26:39 +0200

    Update wektorciagu.R
    m

--- MSzS2lab01/wektorciagu.R
+++ MSzS2lab01/wektorciagu.R
@@ -1,6 +1,6 @@
-n=1:100
+n=1:100  #Tworzy wektor n zawierający liczby od 1 do 100.
 n
-an=n+(1/n)
-mean(an)
-sd(an)
-plot(an)
+an=n+(1/n)  #Tworzy wektor an, którego wartości są sumą odpowiadających elementów wektora n i odwrotności tych elementów.
+mean(an)  #Oblicza średnią arytmetyczną wektora an.
+sd(an) #Oblicza odchylenie standardowe wektora an.
+plot(an) #Rysuje wykres wartości z wektora an
