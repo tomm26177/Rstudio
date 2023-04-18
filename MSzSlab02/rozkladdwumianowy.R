57feb143f5903c2f033ededb10079d4084a06f89
Tomasz Kamiński <104736559+tomm26177@users.noreply.github.com>
2023-04-18 21:38:47 +0200

    Update rozkladdwumianowy.R
    m

--- MSzSlab02/rozkladdwumianowy.R
+++ MSzSlab02/rozkladdwumianowy.R
@@ -2,12 +2,22 @@
 p=0.15
 n=10
 k=0:n #zmienna losowa, może przyjąć takie wartości
-RP=data.frame(k,dbinom(k,n,p))
-plot(RP)
+RP=data.frame(k,dbinom(k,n,p))  #: Tworzy zmienną RP, która jest ramką danych zawierającą dwie kolumny: k i prawdopodobieństwo
+#masy funkcji prawdopodobieństwa dla rozkładu dwumianowego z parametrami n i p, 
+#odpowiadające kolejnym wartościom z wektora k.
+
+
+
+plot(RP)    # Generuje wykres punktowy na podstawie zmiennej RP, gdzie wartości w kolumnie k są ośmi poziomą,
+#a wartości w kolumnie drugiej są ośmi pionową.
+#Wykres ten przedstawia funkcję prawdopodobieństwa dla rozkładu dwumianowego z parametrami n i p.
+
+
+
 
 #P(X=a) prawdopodobienstwo w punkcie a
 a=2
-dbinom(a,n,p)
+dbinom(a,n,p)  #rozkład dwumianowy
 
 
 #prawdopodobieństwo w przedziałach wzory tabela 9
