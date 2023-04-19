bea01e4cf2e974aa3010458c61322dc0832a3f7f
Brzozako <65349652+Brzozako@users.noreply.github.com>
2023-04-19 19:58:04 +0200

    Update SumaRzutuKostka.R

--- MSzSlab05/SumaRzutuKostka.R
+++ MSzSlab05/SumaRzutuKostka.R
@@ -21,3 +21,35 @@ c=3450-0.5
 d=3500+0.5
 PC=pnorm(d,n*m,s*sqrt(n))-pnorm(c,n*m,s*sqrt(n))
 PC
+###############################
+# ustawienie liczby rzutów
+n <- 1000
+
+# średnia arytmetyczna dla jednego rzutu
+m <- 1/6 * (1 + 2 + 3 + 4 + 5 + 6)
+
+# obliczenie wariancji dla jednego rzutu
+EX2 <- 1/6 * (1^2 + 2^2 + 3^2 + 4^2 + 5^2 + 6^2)
+D2X <- EX2 - m^2
+
+# odchylenie standardowe
+s <- sqrt(D2X)
+
+# uwzględnienie poprawki związaną z tym, że zmienna przyjmuje całkowite wartości
+poprawka <- 0.5
+
+# prawdopodobieństwo, że suma wyrzuconych oczek będzie większa niż 3480.5
+a <- 3480 + poprawka
+PA <- pnorm(155000, n * m, s * sqrt(n))
+PA
+
+# prawdopodobieństwo, że suma wyrzuconych oczek będzie większa niż 3550.5
+b <- 3550 + poprawka
+PB <- 1 - pnorm(b, n * m, s * sqrt(n))
+PB
+
+# prawdopodobieństwo, że suma wyrzuconych oczek będzie mieściła się między 3450.5 a 3500.5
+c <- 3450 - poprawka
+d <- 3500 + poprawka
+PC <- pnorm(d, n * m, s * sqrt(n)) - pnorm(c, n * m, s * sqrt(n))
+PC
