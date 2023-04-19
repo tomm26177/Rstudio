b9cd7a938fb8c3aae4f6b7afe48213f248a68cd6
Brzozako <65349652+Brzozako@users.noreply.github.com>
2023-04-19 19:50:23 +0200

    Update LimitWagiWinda.R

--- MSzSlab05/LimitWagiWinda.R
+++ MSzSlab05/LimitWagiWinda.R
@@ -11,3 +11,19 @@ a=1000
 ns=11
 PA=pnorm(a,ns*m,s*sqrt(ns))
 PA
+#####################
+# Ustawiamy parametry rozkładu normalnego N(m, s) dla wagi człowieka
+n = 14  # liczba ludzi
+m = 75  # średnia waga (kg)
+s = 15  # odchylenie standardowe wagi (kg)
+
+# Obliczamy prawdopodobieństwo, że waga 14 osób nie przekroczy 1000 kg
+a = 1000  # górna granica wagi
+PA = pnorm(a, n*m, s*sqrt(n))  # wyliczenie funkcją pnorm()
+print("Prawdopodobieństwo, że waga 14 osób nie przekroczy 1000 kg:", PA)
+
+# Szukamy liczby ludzi (ns), dla której prawdopodobieństwo przekroczenia wagi 1000 kg wynosi 0.001 (czyli P(x<1000)=0.999)
+a = 1000  # górna granica wagi
+P = 0.999  # prawdopodobieństwo nieprzekroczenia wagi 1000 kg
+ns = qnorm(P, m, s/sqrt(n)) / m  # wyliczenie funkcją qnorm() i dostosowanie wyniku do liczby ludzi
+print("Liczba ludzi, dla której prawdopodobieństwo przekroczenia wagi 1000 kg wynosi 0.001:", round(ns))
