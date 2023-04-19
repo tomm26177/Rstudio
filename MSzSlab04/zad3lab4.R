ac621ff331ddec26d80274ab697d3abe9878126b
Brzozako <65349652+Brzozako@users.noreply.github.com>
2023-04-19 19:36:07 +0200

    Update zad3lab4.R

--- MSzSlab04/zad3lab4.R
+++ MSzSlab04/zad3lab4.R
@@ -12,3 +12,22 @@ m=par[1]
 s=par[2]
 m
 s
+##############################################
+#X=N(m=?,s=?) - nieznane parametry rozkładu normalnego
+a1 = 2
+a2 = 10
+p1 = 0.05 # kwantyl rzędu p1
+p2 = 0.9 # kwantyl rzędu p2
+
+# Tworzenie macierzy A i wektora b
+A = matrix(c(1, 1, qnorm(p1), qnorm(p2)), 2, 2)
+b = c(a1, a2)
+
+# Wyznaczanie parametrów rozkładu normalnego
+par = solve(A) %*% b
+m = par[1] # wartość oczekiwana
+s = par[2] # odchylenie standardowe
+
+# Wyświetlenie wyników
+m
+s
