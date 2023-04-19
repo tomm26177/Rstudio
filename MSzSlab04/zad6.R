9f0b6a5ca9eb60f077c25b2bc9b416be8b926ae8
Brzozako <65349652+Brzozako@users.noreply.github.com>
2023-04-19 19:41:24 +0200

    Update zad6.R

--- MSzSlab04/zad6.R
+++ MSzSlab04/zad6.R
@@ -7,3 +7,17 @@ a=20
 pnorm(a,m,s)
 
 qnorm(1-p,m,s)
+#########################
+#X-wysokosc drzewa w metrach
+m=25
+s=4
+p=0.25 #25% wycinamy, zostaje 75%
+
+#P(X<a)
+a=20
+PA=pnorm(a,m,s)
+PA
+
+#kwantyl rzędu 0.75
+t=qnorm(0.75,m,s)
+t
