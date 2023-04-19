446f793735e46c09f44de6c46d307c87016552b7
Brzozako <65349652+Brzozako@users.noreply.github.com>
2023-04-19 19:42:23 +0200

    Update zad7.R

--- MSzSlab04/zad7.R
+++ MSzSlab04/zad7.R
@@ -4,3 +4,10 @@ x71=rnorm(100,m,s)
 mean(x71)
 sd(x71)
 hist(x71)
+######################
+m = 10 # średnia
+s = 4 # odchylenie standardowe
+x71 = rnorm(100, m, s) # generowanie próby losowej rozkładu normalnego
+mean(x71) # obliczenie średniej z próby
+sd(x71) # obliczenie odchylenia standardowego z próby
+hist(x71) # histogram próby
